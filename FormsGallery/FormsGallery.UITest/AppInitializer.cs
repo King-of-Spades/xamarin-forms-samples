afaba357ce58e545c0db21217ec7f804e83d774a
king-of-spades <kegr@microsoft.com>
2018-08-09 11:16:08 -0700

    remove unneeded comments

--- FormsGallery/FormsGallery.UITest/AppInitializer.cs
+++ FormsGallery/FormsGallery.UITest/AppInitializer.cs
@@ -10,17 +10,6 @@ namespace FormsGallery.UITest
     {
         public static IApp StartApp(Platform platform)
         {
-            // TODO: If the iOS or Android app being tested is included in the solution 
-            // then open the Unit Tests window, right click Test Apps, select Add App Project
-            // and select the app projects that should be tested.
-            //
-            // The iOS project should have the Xamarin.TestCloud.Agent NuGet package
-            // installed. To start the Test Cloud Agent the following code should be
-            // added to the FinishedLaunching method of the AppDelegate:
-            //
-            //    #if ENABLE_TEST_CLOUD
-            //    Xamarin.Calabash.Start();
-            //    #endif
             if (platform == Platform.Android)
             {
                 return ConfigureApp
